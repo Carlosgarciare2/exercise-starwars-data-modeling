f0569ba9c14c03e85b7239f48ea7e245c82b9591
Carlosgarciare2 <122974615+Carlosgarciare2@users.noreply.github.com>
2023-05-29 10:27:08 +0000

    generado

--- src/models.py
+++ src/models.py
@@ -7,26 +7,43 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Planets(Base):
+    __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    name = Column(String(250))
+    climate = Column(String(250))
+    diameter = Column(String(250))
+    gravity = Column(String(250))
+    orbital_period = Column(String(250))
+    population = Column(String(250))
+    terrain = Column(String(250))
+    
+class Characters(Base):
+    __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
+    eye_color = Column(String(250))
+    hair_color = Column(String(250))
+    skin_color = Column(String(250))
+    gender = Column(String(250))
+    height = Column(String(250))
+    birth_year = Column(String(250))
 
-    def to_dict(self):
-        return {}
+class Favorites(Base):
+    __tablename__= 'favorites'
+    id = Column(Integer, primary_key=True)
+    planet_id: Column(Integer, ForeignKey('planet.id'))
+    character_id: Column(Integer, ForeignKey('character.id'))
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(130), nullable=False)
+    email = Column(String(130), nullable=False)
+    password = Column(String(130), nullable=False)
+    favorites_id = Column (Integer, ForeignKey('favorites.id'))
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
